25bbe95124bbb8f2ff845ee17707a130a06f5522
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-10-01 18:10:05 -0400

    passes all specs

--- models/user.js
+++ models/user.js
@@ -3,8 +3,33 @@ const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { loggi
 
 const User = db.define('user', {
 // YOUR CODE HERE...
+    first: { type: Sequelize.STRING },
+    last: { type: Sequelize.STRING },
+    age: {
+        type: Sequelize.INTEGER,
+        validate: { min: 18 }
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    bio: { type: Sequelize.TEXT }
 }, {
 // ...AND HERE
+    getterMethods: {
+        fullName: function() {
+            return this.first + ' ' + this.last;
+        }
+    }
+
 });
+
+User.prototype.haveBirthday = function() {
+    return User.findById(1)
+        .then(function(user) {
+            return user.increment('age', {by: 1});
+        });
+};
+
 
 module.exports = User;
